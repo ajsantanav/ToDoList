b35797a9423c48a5cbe54546bfba98e53087d62e
ajsantanav <adriansantana.v@gmail.com>
2025-01-20 18:42:16 -0500

    working 1

--- src/component/ToDo.jsx
+++ src/component/ToDo.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import  {useState } from 'react'
 
-function ToDo() {
+function ToDo(props) {
   return (
-    <div>ToDo</div>
+    <div>
+      <h1>To do List</h1>
+
+      <form>
+      <h2 className="">
+          <label className="">
+            Add your task:
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="">Add</button>
+      </form>
+    </div>
   )
 }
 
